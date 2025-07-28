dba185e76aef93b4ba84a0dba027fcbf160f7f47
IntelliWeb <deventerprisedtb@gmail.com>
2025-07-28 20:06:58 +0200

    Añadir @tailwindcss/postcss y actualizar configuración

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // CAMBIO: Usamos el nuevo paquete
     autoprefixer: {},
   },
 };
