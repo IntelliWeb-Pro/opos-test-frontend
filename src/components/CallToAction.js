82fd18d66165e9a0792dbc6b280747958909ea89
IntelliWeb <deventerprisedtb@gmail.com>
2025-08-12 20:27:25 +0200

    Test de cambio de color botones

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -34,7 +34,7 @@ export default function CallToAction() {
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <Link
               href="/precios"
-              className="group inline-flex items-center justify-center rounded-md bg-yellow-500 px-6 py-3 text-base font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-colors"
+              className="group inline-flex items-center justify-center rounded-md bg-yellow-500 px-6 py-3 text-base font-semibold text-white shadow-sm hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary transition-colors"
             >
               Empezar prueba gratuita
               <ArrowIcon />
